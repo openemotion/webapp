3651fecf748ead411a3b070ee9d4edd94e91fe5e
Eli Finer <elig@il.ibm.com>
2012-11-09 00:59:29 +0200

    created a simple app with stub pages

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+if os.getenv("MODE") == "production":
+    DATABASE = "/home/dotcloud/current/data.db"
+    SECRET_KEY = "8DC1DE50136CA4F4217E928893CA4866"
+else:
+    DATABASE = "data.db"
+    SECRET_KEY = "development key"
+    DEBUG = True
---
+++ webapp.py
@@ -0,0 +1,32 @@
+import sqlite3
+from flask import Flask, render_template, request, g
+
+app = Flask(__name__)
+app.config.from_object("config")
+
+@app.route("/")
+def main():
+    return render_template("main.html")
+
+@app.route("/faq")
+def faq():
+    return render_template("faq.html")
+
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/c/<id>")
+def conversation(id):
+    return render_template("conversation.html")
+
+@app.route("/profile/<id>")
+def profile(id):
+    return render_template("profile.html")
+
+if __name__ == '__main__':
+    app.run()
