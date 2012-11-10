73193559aa150d20ab5cfba3c956e906016e4801
Eli Finer <elig@il.ibm.com>
2012-11-10 14:57:42 +0200

    fixed dotcloud issues

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 if os.getenv("MODE") == "production":
     DATABASE = "/home/dotcloud/current/data.db"
     SECRET_KEY = "8DC1DE50136CA4F4217E928893CA4866"
+    DEBUG = True
 else:
     DATABASE = "data.db"
     SECRET_KEY = "development key"
