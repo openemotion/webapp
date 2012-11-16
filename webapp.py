b975428925da68b87ffb7020774f3f3a6aa683a7
Eli Finer <elig@il.ibm.com>
2012-11-16 17:23:41 +0200

    removed separate title box

--- webapp.py
+++ webapp.py
@@ -74,7 +74,8 @@ def new_conversation():
     if not session["logged_in_user"]:
         abort(401)
     if request.method == "POST":
-        id = g.db.conversations.save(session["logged_in_user"], request.form["title"])
+        title = utils.extract_title(request.form["message"])
+        id = g.db.conversations.save(session["logged_in_user"], title)
         g.db.messages.save(id, session["logged_in_user"], request.form["message"])
         return redirect(url_for("conversation", id=id))
     else:
