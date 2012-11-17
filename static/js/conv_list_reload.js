b3e484f546c7675eaf391c164cc83db586e7cc03
Eli Finer <elig@il.ibm.com>
2012-11-17 21:38:40 +0200

    css at top of file, js at bottom

--- static/js/conv_list_reload.js
+++ static/js/conv_list_reload.js
@@ -1,6 +1,7 @@
 $(function() {
     function reloadConversationList() {
         $.ajax("conversations", {
+            ifModified: true,
             success: function (data, textStatus, jqXHR) {
                 $("#conversation_list").empty().append(data);
                 console.log("updating...");
