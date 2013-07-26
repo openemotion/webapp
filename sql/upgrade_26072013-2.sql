0cac4feffe2911e8aa907730645c3d8628df946f
Eli Finer <eli.finer@gmail.com>
2013-07-26 14:40:05 +0300

    sql fix

--- sql/upgrade_26072013-2.sql
+++ sql/upgrade_26072013-2.sql
@@ -1,2 +1,2 @@
 alter table conversations add show_on_landing boolean;
-update conversations set show_on_landing = 1 where id in (5,17,20,26);
+update conversations set show_on_landing = true where id in (5,17,20,26);
