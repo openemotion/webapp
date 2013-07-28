21d1fca0ee51319c23c3b9d35b0fa7e516decc0f
Eli Finer <eli.finer@gmail.com>
2013-07-28 22:58:20 +0300

    less frequent reloads

--- config/prod.py
+++ config/prod.py
@@ -5,5 +5,5 @@ POSTMARK_API_KEY = os.environ['POSTMARK_API_KEY']
 ENCRYPT_KEY = os.environ['OPENEM_ENCRYPT_KEY']
 SECRET_KEY = os.environ['FLASK_SECRET_KEY']
 ALWAYS_EMAIL = ["eli.finer@gmail.com", "yaelfiner@gmail.com"]
-UPDATE_INTERVAL = 5000
+UPDATE_INTERVAL = 1000 * 60
 DEBUG = False
